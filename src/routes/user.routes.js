a1944e72fab9c6fe15fe0d7274768f6c5e056eb4
Yashshah-0702 <yash722002@gmail.com>
2024-01-24 21:24:45 +0530

    removed:authentication

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,7 @@ const { signUp , login } = require("../controller/user.controller");
 const isAuthenticated = require('../middleware/authentication.middelware')
 
 module.exports = () => {
-  router.post("/signUp/:user",isAuthenticated, signUp);
+  router.post("/signUp/:user", signUp);
   router.post("/login",login)
   return router;
 };
